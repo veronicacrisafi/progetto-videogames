026f345ea275d1f8296cc022d45f53cb9c30c354
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-25 20:18:52 +0100

    Sostituito l'uso di Faker con dati statici per il seeder VideogamesTableSeeder

--- database/seeders/VideogamesTableSeeder.php
+++ database/seeders/VideogamesTableSeeder.php
@@ -12,15 +12,47 @@ class VideogamesTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(Faker $faker): void
+    public function run(): void
     {
-        //prova con faker e ciclo for
-        for ($i = 0; $i < 10; $i++) {
+        $videogiochiHorror = [
+            [
+                'titolo_videogame' => 'Resident Evil',
+                'descrizione_videogame' => 'Un classico survival horror con zombie e misteri.',
+                'anno_videogame' => 1996,
+            ],
+            [
+                'titolo_videogame' => 'Silent Hill',
+                'descrizione_videogame' => 'Un horror psicologico ambientato in una città nebbiosa.',
+                'anno_videogame' => 1999,
+            ],
+            [
+                'titolo_videogame' => 'Outlast',
+                'descrizione_videogame' => 'Un survival horror in prima persona in un manicomio abbandonato.',
+                'anno_videogame' => 2013,
+            ],
+            [
+                'titolo_videogame' => 'Call of Duty',
+                'descrizione_videogame' => 'Uno sparatutto in prima persona ambientato in scenari di guerra realistica.',
+                'anno_videogame' => 2003,
+            ],
+            [
+                'titolo_videogame' => 'God of War',
+                'descrizione_videogame' => 'Un action-adventure mitologico con combattimenti epici e una forte componente narrativa.',
+                'anno_videogame' => 2005,
+            ],
+            [
+                'titolo_videogame' => 'Dead Space',
+                'descrizione_videogame' => 'Un horror fantascientifico in terza persona ambientato nello spazio profondo.',
+                'anno_videogame' => 2008,
+            ],
+
+        ];
+        foreach ($videogiochiHorror as $gioco) {
             $newVideogame = new Videogame();
-            $newVideogame->titolo_videogame = $faker->sentence(2);
-            $newVideogame->descrizione_videogame = $faker->paragraph();
-            $newVideogame->anno_videogame = $faker->numberBetween(1980, 2026);
+            $newVideogame->titolo_videogame = $gioco['titolo_videogame'];
+            $newVideogame->descrizione_videogame = $gioco['descrizione_videogame'];
+            $newVideogame->anno_videogame = $gioco['anno_videogame'];
             $newVideogame->save();
-        };
+        }
     }
 }
