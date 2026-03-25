b22ff97d642f3b3ef8e00923e12c5c6783e41623
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-25 19:56:28 +0100

    Aggiornato il DatabaseSeeder per utilizzare VideogamesTableSeeder e implementata la generazione di dati fittizi nel VideogamesTableSeeder.

--- database/seeders/VideogamesTableSeeder.php
+++ database/seeders/VideogamesTableSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Videogame;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class VideogamesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        //prova con faker e ciclo for
+        for ($i = 0; $i < 10; $i++) {
+            $newVideogame = new Videogame();
+            $newVideogame->titolo_videogame = $faker->sentence(2);
+            $newVideogame->descrizione_videogame = $faker->paragraph();
+            $newVideogame->anno_videogame = $faker->numberBetween(1980, 2026);
+            $newVideogame->save();
+        };
     }
 }
