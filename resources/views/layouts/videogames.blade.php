2e85cd4bf5e547c8fac43d78d4e2a70ad431bacd
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-25 20:04:52 +0100

    Aggiunta il layout base per la vista Videogames

---
+++ resources/views/layouts/videogames.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>Videogames</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>
+            @yield('titolo')
+        </h1>
+        @yield('contenuto')
+    </div>
+
+</body>
+
+</html>
