ed9de6deca2242020adadb5f9c1af6ac56305381
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-25 19:28:09 +0100

    Aggiunto il DashboardController e le rotte associate per la sezione admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/index', [DashboardController::class, 'index'])
+            ->name('index');
+    });
+
+require __DIR__ . '/auth.php';
