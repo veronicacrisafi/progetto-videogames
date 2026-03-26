25dcb66cef0a1329b19c94f70d39bee7cac66cd8
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-26 22:44:19 +0100

    Aggiunta la chiamata al GenresTableSeeder nel DatabaseSeeder per popolare i generi di videogiochi.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);*/
-        $this->call([VideogamesTableSeeder::class]);
+        $this->call([VideogamesTableSeeder::class, GenresTableSeeder::class]);
     }
 }
