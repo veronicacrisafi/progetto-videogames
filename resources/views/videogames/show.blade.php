a95e7a38ebac824ecdee021dd3f7cc710d722790
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-26 19:45:54 +0100

    Aggiunta la logica per la conferma di eliminazione di un videogioco tramite un modal nella vista show

--- resources/views/videogames/show.blade.php
+++ resources/views/videogames/show.blade.php
@@ -11,14 +11,33 @@
                     <p class="card-text">Descrizione: {{ $videogame->descrizione_videogame }}</p>
                     <p class="card-text">Anno di uscita: {{ $videogame->anno_videogame }}</p>
                     <div class="d-flex py-4">
-                        <a href="{{ route('videogames.edit', $videogame) }}" class="btn btn-outline-warning">Modifica</a>
-                        <form action="{{ route('videogames.destroy', $videogame) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <input type="submit" class="btn btn-outline-danger" value="Elimina">
-                        </form>
-
+                        <a href="{{ route('videogames.edit', $videogame) }}" class="btn btn-outline-warning w-50">Modifica</a>
+                        <button type="button" class="btn btn-outline-danger w-50" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">
+                            Elimina
+                        </button>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Vuoi eliminare la tipologia?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{ route('videogames.destroy', $videogame) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" class="btn btn-outline-danger" value="Elimina">
+                    </form>
                 </div>
             </div>
         </div>
