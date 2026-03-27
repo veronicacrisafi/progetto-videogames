5bdd4dfade0b04efafe3295bbaf38edf759827d9
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-27 22:13:17 +0100

    Aggiunta la chiamata al seeder per la tabella Developers nel DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             VideogamesTableSeeder::class,
             GenresTableSeeder::class,
             ConsoleTableSeeder::class,
+            DevelopersTableSeeder::class,
 
         ]);
     }
