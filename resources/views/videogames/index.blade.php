d72e2e7dad592718527125d346ae364ac5fd1df0
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-27 20:17:22 +0100

    Modificata la visualizzazione dei videogiochi per includere i generi associati nella scheda di dettaglio

--- resources/views/videogames/index.blade.php
+++ resources/views/videogames/index.blade.php
@@ -8,7 +8,15 @@
             <div class="col-4 my-3">
                 <div class="card h-100 d-flex flex-column">
                     <div class="card-body d-flex flex-column">
-                        <h4 class="card-title mb-5">{{ $videogame->titolo_videogame }}</h4>
+                        <h4 class="card-title mb-3">{{ $videogame->titolo_videogame }}</h4>
+                        <div class="mb-3">
+                            @if (count($videogame->genres) > 0)
+                                @foreach ($videogame->genres as $genre)
+                                    <span class="badge"
+                                        style="background-color: {{ $genre->colore }}">{{ $genre->genere_videogame }}</span>
+                                @endforeach
+                            @endif
+                        </div>
                         <h5 class="card-subtitle mb-4 text-color-secondary">{{ $videogame->descrizione_videogame }}</h5>
                         <h6 class="card-subtitle mb-3">Anno: {{ $videogame->anno_videogame }}</h6>
                         <div class="mt-auto">
