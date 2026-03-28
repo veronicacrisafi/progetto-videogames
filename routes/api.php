8f148b6d0b97780d50b49c4202c8469e0cdb236a
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-28 11:34:03 +0100

    Rimosso il codice commentato per la rotta utente e mantenuta la rotta per i videogiochi/api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\VideogameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('videogames', [VideogameController::class]);
